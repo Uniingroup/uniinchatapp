bf3c112a48400105dbd5a151e373888a5cf8ab28
Uniingroup <167407218+Uniingroup@users.noreply.github.com>
2025-06-28 18:47:08 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,21 @@
-// Firebase config will go here
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// Same exact config
+const firebaseConfig = {
+  apiKey: "AIzaSyBkr3uTfSqeeShHccsYH9B8iDKPWNTbAAY",
+  authDomain: "uniinchatapp.firebaseapp.com",
+  projectId: "uniinchatapp",
+  storageBucket: "uniinchatapp.firebasestorage.app",
+  messagingSenderId: "362376051567",
+  appId: "1:362376051567:web:eb8bf76bdf51d5aac9869a",
+  measurementId: "G-H3BJG5WCZQ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
